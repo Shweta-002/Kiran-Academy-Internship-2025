41beddbf9bbe7613e2a227773c16cd11b45f0192
Shweta-002 <shwetapatil2247@gmail.com>
2025-04-20 19:36:26 +0530

    Deleted abstraction files

--- Animal.java
+++
@@ -1,9 +0,0 @@
-package com.abstraction_17th_april;
-
-public  interface Animal {
-	
-	void sound();
-	void color();
-	
-
-}
--- OnlinePayment.java
+++
@@ -1,7 +0,0 @@
-package com.abstraction_17th_april;
-
-public interface OnlinePayment {
-	
-	public abstract void makePayment();
-
-}
